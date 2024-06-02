c553b5e3ed1113d5eadc82a3d4303976866ffde8
shehroonhasnain <hasnainshehroon@gmail.com>
2024-06-02 04:08:32 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// Functions
+// 1. Write a js program to find cube of any number using function.
+// function findCube(number) {
+//     return number * number * number;
+//   }
+//   var number=prompt("Enter any value:");
+//   var cube=findCube(number);
+//   console.log(`Cube of ${number} is ${cube}`);
+
+// 2. Write a js program to find diameter, circumference and area of circle using functions.
+// function findDiameter(radius) {
+//     return 2 * radius;
+// }
+// function findCircumference(radius) {
+//     return 2 * 3.1415 * radius;
+// }
+// function findArea(radius) {
+//     return Math.PI * radius * radius;
+// }
+// var radius=prompt("Enter radius:");
+// var diameter=findDiameter(radius);
+// var circumference=findCircumference(radius);
+// var area=findArea(radius);
+// console.log(`Radius:${radius},Diameter:${diameter},Circumferene:${circumference},Area:${area}`);
+
+// 3. Write a js program to find maximum and minimum between two numbers using functions.
+// function maximum(num1,num2){
+//     if(num1>num2){
+//         return num1;
+//     }else{
+//         return num2;
+//     }
+// }
+// function minimum(num1,num2){
+//     if(num1<num2){
+//         return num1;
+//     }else{
+//         return num2;
+//     }
+// }
+// var num1=prompt("Enter first num:");
+// var num2=prompt("Enter second num:");
+// var maximumNum=maximum(num1,num2);
+// var minimumNum=minimum(num1,num2);
+// console.log(`Maximum number:${maximumNum},Minimum number:${minimumNum}`);
+
+// 4. Write a js program to check whether a number is even or odd using functions.
+// function evenOrodd(num){
+//     if(num%2==0){
+//         console.log(`Number ${num} is even`);
+//     }else{
+//         console.log(`Number ${num} is odd`);
+//     }
+// }
+// var num=prompt("Enter a value:");
+// evenOrodd(num);
+
